b9c1cbbfbb000a184fda0afe58ac936761f8e8e3
YaKartoshka <100137723+YaKartoshka@users.noreply.github.com>
2022-10-25 19:00:05 +0600

    commit

--- public/js/my_profile.js
+++ public/js/my_profile.js
@@ -1,5 +1,5 @@
 var stories_list=document.querySelector('.my_stories');
-
+var clicked_id=0;
 
 
 
@@ -64,23 +64,50 @@ async function showMyStories(){
         var newText = document.createElement("p");
         var newTitle = document.createElement("div");
         var newAuthor = document.createElement("div");
-        
-            
-
+        var newDel = document.createElement("div");
+        var btnClose = document.createElement("button");    
+        var newLink = document.createElement("a");
+        btnClose.classList.add('delbtn');
+        btnClose.id = 'remove';    
         newDiv.classList.add('container-lg')
         newDiv.classList.add('story')
         newDiv.classList.add('bg-primary');
         newDiv.id = doc.id;
+        newLink.classList.add('fa-solid')
+        newLink.classList.add('fa-circle-xmark')
+        newDel.id=doc.id;    
         newText.classList.add('story_content');
         newTitle.classList.add('story_title');
         newAuthor.classList.add('story_author');
         newText.innerHTML = story_text;
-    
+        newDel.classList.add('delete');
+        newDel.setAttribute('onclick', "removeStorie(this.id)");    
+        btnClose.innerHTML = newLink.outerHTML;
+        newDel.innerHTML = btnClose.outerHTML;
         newAuthor.innerHTML = author;
         newTitle.innerHTML = story_title
-        newDiv.innerHTML = newTitle.outerHTML + newText.outerHTML + newAuthor.outerHTML;
+        newDiv.innerHTML = newTitle.outerHTML + newText.outerHTML + newAuthor.outerHTML+newDel.outerHTML;
         stories_list.insertAdjacentElement('afterbegin',newDiv);
+        $('.story').on("click", function () {
+            delId = ($(this).attr('id'));
+            
+        })
         }
    });
 }
 showMyStories();
+
+
+ 
+function removeStorie(click_id){
+    
+    const child_card=document.getElementById(click_id);
+    child_card.remove();
+    
+    fdb.collection('stories').doc(`${click_id}`).delete().then(() => {
+       
+    }).catch((error) => {
+        console.error("Error removing document: ", error);
+    });
+    
+}
